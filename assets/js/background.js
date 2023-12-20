861355b5c108c7eb5ebda0bdfe2012a67a8e1283
Alfredo.Filigheddu <Alfredo Filigheddu@abirk.it>
2023-12-20 15:31:12 +0100

    feat: bg scroll animation

--- assets/js/background.js
+++ assets/js/background.js
@@ -12,27 +12,34 @@ setTimeout(() => {
 }, 3000);
 
 window.addEventListener("scroll", () => {
-  animateBg();
   animateBgWrapper(BG_WRAPPER_LIST[2], 0, 900, 0, 100);
   if (window.innerWidth > 991) {
+    animateBgLogo(55, 2);
     animateBgWrapper(BG_WRAPPER_LIST[0], 1300, 1800, 0, 100);
   } else {
+    animateBgLogo(140, 3.5);
     animateBgWrapper(BG_WRAPPER_LIST[0], 0, 300, 0, 100);
+  }
+
+  if (scrollY >= 1200) {
+    BG_WRAPPER_LIST[2].style.opacity = 0;
+  } else {
+    BG_WRAPPER_LIST[2].style.opacity = 1;
   }
 });
 
-function animateBg() {
+function animateBgLogo(maxTranslate, maxScale) {
   let elHeight = window.document.querySelector(".content-wrapper").getBoundingClientRect().height;
   let aboutTop = window.document.querySelector("#about").getBoundingClientRect().top;
-  let translateX = scale(window.scrollY, 0, elHeight / 6, 140, 0);
-  let scaleDim = scale(window.scrollY, 0, elHeight / 6, 3.5, 1);
+  let translateX = scale(window.scrollY, 0, elHeight / 6, maxTranslate, 0);
+  let scaleDim = scale(window.scrollY, 0, elHeight / 6, maxScale, 1);
   if (translateX <= 0) {
     translateX = 0;
     scaleDim = 1;
   }
   if (aboutTop < 0) {
-    translateX = 140;
-    scaleDim = 3.5;
+    translateX = maxTranslate;
+    scaleDim = maxScale;
   }
   LOGO_EL.style.transform = `translate(${translateX}%, 0%) scale(${scaleDim})`;
 }
